09964e6e4b87f5ed9b89c0198e6c91a2c09c07f5
Tobi Olajumoke <82091923+TobiOlajumoke@users.noreply.github.com>
2023-03-04 11:57:51 +0100

    Update home_activities.py

--- backend-flask/services/home_activities.py
+++ backend-flask/services/home_activities.py
@@ -1,18 +1,20 @@
 from datetime import datetime, timedelta, timezone
 from opentelemetry import trace
+# import logging  
 
 tracer = trace.get_tracer("home.activities")
 
 class HomeActivities:
   def run():
-    with tracer.start_as_current_span("home-activites-mock-data"):
+    # logger.info("HomeActivities")
+    with tracer.start_as_current_span("home-activities-mock-data"):
       span = trace.get_current_span()
       now = datetime.now(timezone.utc).astimezone()
       span.set_attribute("app.now", now.isoformat())
       results = [{
         'uuid': '68f126b0-1ceb-4a33-88be-d90fa7109eee',
         'handle':  'Andrew Brown',
-        'message': 'Cloud is very fun!',
+        'message': 'Cloud is fun!',
         'created_at': (now - timedelta(days=2)).isoformat(),
         'expires_at': (now + timedelta(days=5)).isoformat(),
         'likes_count': 5,
