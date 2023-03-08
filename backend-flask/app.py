af80b60e940fd3ba0db6e68c5a2a10f4a11e19fd
Tobi <olajumoketobi01@gmail.com>
2023-03-08 17:46:05 +0100

    fix xray app issue
    Signed-off-by: Tobi <olajumoketobi01@gmail.com>

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -35,7 +35,7 @@ provider.add_span_processor(processor)
 # X-RAY --------
 xray_url = os.getenv("AWS_XRAY_URL")
 xray_recorder.configure(service='Cruddur', dynamic_naming=xray_url)
-XRayMiddleware(app, xray_recorder)
+
 
 
 # Show this in the logs within the backend-flask app (STDOUT)
@@ -46,6 +46,8 @@ trace.set_tracer_provider(provider)
 tracer = trace.get_tracer(__name__)
 
 app = Flask(__name__)
+# X-RAY ----
+XRayMiddleware(app, xray_recorder)
 
 # HoneyComb ---------
 # Initialize automatic instrumentation with Flask
