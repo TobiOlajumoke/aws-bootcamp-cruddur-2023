42a60a3111a820835f584531629cfa92ebdc5f56
Tobi Olajumoke <Olajumoketobi01@gmail.com>
2023-03-29 12:21:12 +0000

    changed some results and added cognito user id

--- backend-flask/services/home_activities.py
+++ backend-flask/services/home_activities.py
@@ -1,20 +1,20 @@
 from datetime import datetime, timedelta, timezone
 from opentelemetry import trace
-# import logging  
 
 tracer = trace.get_tracer("home.activities")
 
 class HomeActivities:
-  def run():
-    # logger.info("HomeActivities")
-    with tracer.start_as_current_span("home-activities-mock-data"):
+  def run(cognito_user_id=None):
+    #logger.info("HomeActivities")
+    with tracer.start_as_current_span("home-activites-mock-data"):
       span = trace.get_current_span()
       now = datetime.now(timezone.utc).astimezone()
       span.set_attribute("app.now", now.isoformat())
+
       results = [{
         'uuid': '68f126b0-1ceb-4a33-88be-d90fa7109eee',
-        'handle':  'Andrew Brown',
-        'message': 'Cloud is fun!',
+        'handle':  'Voldermort',
+        'message': 'The boy who lived as come to die!',
         'created_at': (now - timedelta(days=2)).isoformat(),
         'expires_at': (now + timedelta(days=5)).isoformat(),
         'likes_count': 5,
@@ -23,8 +23,8 @@ class HomeActivities:
         'replies': [{
           'uuid': '26e12864-1c26-5c3a-9658-97a10f8fea67',
           'reply_to_activity_uuid': '68f126b0-1ceb-4a33-88be-d90fa7109eee',
-          'handle':  'Worf',
-          'message': 'This post has no honor!',
+          'handle':  '9jaBoy',
+          'message': 'Sapa na bad thing!',
           'likes_count': 0,
           'replies_count': 0,
           'reposts_count': 0,
@@ -33,8 +33,8 @@ class HomeActivities:
       },
       {
         'uuid': '66e12864-8c26-4c3a-9658-95a10f8fea67',
-        'handle':  'Worf',
-        'message': 'I am out of prune juice',
+        'handle':  'Tobi',
+        'message': 'I am gonna make it outta this bootcamp and emerge a monster',
         'created_at': (now - timedelta(days=7)).isoformat(),
         'expires_at': (now + timedelta(days=9)).isoformat(),
         'likes': 0,
@@ -42,13 +42,26 @@ class HomeActivities:
       },
       {
         'uuid': '248959df-3079-4947-b847-9e0892d1bab4',
-        'handle':  'Garek',
-        'message': 'My dear doctor, I am just simple tailor',
+        'handle':  'Victhor',
+        'message': 'Kepp Grinding',
         'created_at': (now - timedelta(hours=1)).isoformat(),
         'expires_at': (now + timedelta(hours=12)).isoformat(),
         'likes': 0,
         'replies': []
       }
       ]
+
+      if cognito_user_id != None:
+        extra_crud = {
+          'uuid': '248959df-3079-4947-b847-9e0892d1bab4',
+          'handle':  'Lore',
+          'message': 'My dear brother, it the humans that are the problem',
+          'created_at': (now - timedelta(hours=1)).isoformat(),
+          'expires_at': (now + timedelta(hours=12)).isoformat(),
+          'likes': 1042,
+          'replies': []
+        }
+        results.insert(0,extra_crud)
+
       span.set_attribute("app.result_length", len(results))
       return results
