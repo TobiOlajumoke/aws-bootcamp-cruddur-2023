f58284d96d69834cca2151c8b0a72a62518613f4
Tobi Olajumoke <Olajumoketobi01@gmail.com>
2023-04-04 22:59:24 +0000

    schema setup

--- backend-flask/db/schema.sql
+++ backend-flask/db/schema.sql
@@ -1,10 +1,25 @@
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
-postgresql://[user[:password]@][netloc][:port][/dbname][?param1=value1&...]
+-- forcefully drop our tables if they already exist
+DROP TABLE IF EXISTS public.users cascade;
+DROP TABLE IF EXISTS public.activities;
 
-export CONNECTION_URL="postgresql://postgres:password@localhost:5432/cruddur"
+CREATE TABLE public.users (
+  uuid UUID default uuid_generate_v4() primary key,
+  display_name text,
+  handle text,
+  cognito_user_id text,
+  created_at timestamp default current_timestamp NOT NULL
+);
 
-gp env CONNECTION_URL="postgresql://postgres:password@localhost:5432/cruddur"
-
-export PROD_CONNECTION_URL="postgresql://cruddurroot:Zabuza007@cruddur-db-instance.cybtgwxoa2s0.us-east-1.rds.amazonaws.com"
-
-gp env PROD_CONNECTION_URL="postgresql://cruddurroot:Zabuza007@cruddur-db-instance.cybtgwxoa2s0.us-east-1.rds.amazonaws.com"
+CREATE TABLE public.activities (
+  uuid UUID default uuid_generate_v4() primary key,
+  user_uuid UUID REFERENCES public.users(uuid) NOT NULL,
+  message text NOT NULL,
+  replies_count integer default 0,
+  reposts_count integer default 0,
+  likes_count integer default 0,
+  reply_to_activity_uuid integer,
+  expires_at timestamp,
+  created_at timestamp default current_timestamp NOT NULL
+);
